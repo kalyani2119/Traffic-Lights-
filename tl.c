5a1aef80d18a2ca744062b9a208c5959ba24a046
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-01 22:44:31 +0530

    Update tl.c

--- tl.c
+++ tl.c
@@ -1,2 +1,26 @@
-# Traffic-Lights-
-An embedded C program to design traffic lights using 8051 microcontroller
+//Traffic-Lights-
+//An embedded C program to design traffic lights using 8051 microcontroller
+#include<reg51.h>
+sbit red = p1^0;
+sbit yellow = p1^1;
+sbit green = p1^2;
+void main()
+{
+  unsigned int i;
+  red=0;
+  yellow=0;
+  green=0;
+  while(1)
+  {
+    red = 1;
+    for(i=0;i<100000;i++);
+    red = 0;
+    yellow = 1;
+    for(i=0;i<50000:i++);
+    yellow = 0;
+    green = 1;
+    for(i=0;i<100000;i++);
+    green = 0;
+  }
+}
+  
